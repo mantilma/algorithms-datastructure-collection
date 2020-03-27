08629f1175f291a0d1b99a8e5d10a020f44e1264
marco <amantil@protonmail.com>
2020-03-27 21:58:42 +0100

    Climb stairs dynamic programming

---
+++ src/algorithms/dinamicprogramming/ClimbStairs.java
@@ -0,0 +1,70 @@
+package algorithms.dinamicprogramming;
+
+/*
+ * You are climbing a stair case. It takes n steps to reach to the top.
+
+	Each time you can either climb 1 or 2 or 3 steps. In how many distinct ways can you climb to the top?
+
+	Note: Given n will be a positive integer.
+
+	Example 1:
+
+	Input: 2
+	Output: 2
+	Explanation: There are two ways to climb to the top.
+	1. 1 step + 1 step
+	2. 2 steps
+
+	Example 2:
+
+	Input: 3
+	Output: 3
+	Explanation: There are three ways to climb to the top.
+	1. 1 step + 1 step + 1 step
+	2. 1 step + 2 steps
+	3. 2 steps + 1 step
+
+
+ * 
+ */
+public class ClimbStairs {
+	//ricorsion: complessita temporaleO(3^n), spaziale O(n) 
+	public static int climbStairs (int scale, int passi) {
+		if (passi > scale) return 0;
+		if (passi == scale) return 1;
+		
+		return climbStairs(scale, passi + 1) + climbStairs(scale, passi + 2) + climbStairs(scale, passi + 3);
+	}
+	
+	//miglioramento della soluzione ricorsiva con memoization: complessita temporaleO(n), spaziale O(n)
+	public static int climbStairsMem (int scale, int passi, int memo[]) {
+		if (passi > scale) return 0;
+		if (passi == scale) return 1;
+		if (memo[passi] > 0) return memo[passi];
+		return memo[passi] = climbStairs(scale, passi + 1) + climbStairs(scale, passi + 2) + climbStairs(scale, passi + 3);
+	}
+	
+	//approccio bottom up, costruisco array dal bassa verso l'alto iterativamente: o(n) o(n)
+	public static int climbStairsBottomUp (int scale) {
+		if (scale == 1) {
+			return 1;
+		}
+		int[] dp = new int[scale + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		for(int i = 4; i <= scale; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+		return dp[scale];
+		
+	}
+	
+	
+	
+	public static void main (String[] args) {
+		System.out.println(climbStairs(5,0));
+		System.out.println(climbStairsMem(5,0,new int[6]));
+		System.out.println(climbStairsBottomUp(5));
+	}
+}
--- src/algorithms/dinamicprogramming/fibonacci.java
+++ src/algorithms/dinamicprogramming/fibonacci.java
@@ -1,15 +1,18 @@
 package algorithms.dinamicprogramming;
 
 public class fibonacci {
+	
+	//memoization top-up. parto da f(5) e richiamo ricorsivamente f(4) + f(3) ecc...
 	public static Integer fibonacciTopUpRecursive (Integer number, Integer[] temp) {
 		if (temp[number] != null) return temp[number];
 		if (number == 1 || number == 2) return 1;
-		else temp[number] = fibonacciTopUpRecursive(number - 1, temp) + fibonacciTopUpRecursive(number - 2, temp);
-		return temp[number];
+		else return temp[number] = fibonacciTopUpRecursive(number - 1, temp) + fibonacciTopUpRecursive(number - 2, temp);
 		
 	}
 	
-	public static Integer fibonacciBottomUpRecursive (Integer number) {
+	//iterative bottom-up: la soluzione n dipende da n-1 e n-2, n-1 da n-2 e n-3. Quindi dal basso verso l'alto
+	//metto nell'array i casi base e poi ciclo for calcolo tutte le altre casella dell'array.
+	public static Integer fibonacciBottomUp (Integer number) {
 		if (number == 1 || number == 2) return 1;
 		Integer[] temp = new Integer[number + 1];
 		temp[1] = 1;
@@ -22,6 +25,6 @@ public class fibonacci {
 	
 	public static void main (String args[]) {
 		System.out.println(fibonacciTopUpRecursive(8, new Integer[9]));
-		System.out.println(fibonacciBottomUpRecursive(8));
+		System.out.println(fibonacciBottomUp(8));
 	}
 }
