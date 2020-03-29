d26c1c8275dc494dd811e5268658684f7158a8ca
marco <amantil@protonmail.com>
2020-03-29 13:03:36 +0200

    aggiunto commento

--- src/algorithms/dinamicprogramming/BuySellStocks.java
+++ src/algorithms/dinamicprogramming/BuySellStocks.java
@@ -1,5 +1,7 @@
 package algorithms.dinamicprogramming;
 
+import java.util.Arrays;
+
 /*
  * Say you have an array for which the ith element is the price of a given stock on day i.
  *
@@ -23,6 +25,58 @@ package algorithms.dinamicprogramming;
  *
  */
 public class BuySellStocks {
+	
+	//soluzione ricorsiva top down. t(n) = 2T(n/2) + O(n) = O(nlgn)
+	//O(n) è dovuto ai calcoli dei max per ogni chiamata.
+	//complessità spaziale O(lgn) 
+	public static int BuySellStockTopDown(int[] prices, int indexLow, int indexHigh) {
+		if (indexLow >= indexHigh) return 0;
+		
+		int index_medium = (indexLow + indexHigh) / 2;
+		int profitLeft = BuySellStockTopDown(prices, indexLow, index_medium);
+		int profitRight = BuySellStockTopDown(prices, index_medium + 1, indexHigh);
+		
+		int minLeft = Integer.MAX_VALUE;
+		for (int i = indexLow; i <= index_medium; i++) {
+			minLeft = prices[i] < minLeft ? prices[i] : minLeft;
+		}
+		int maxRight = Integer.MIN_VALUE;
+		for (int i = index_medium + 1; i <= indexHigh; i++) {
+			maxRight = prices[i] > maxRight ? prices[i] : maxRight;
+		}
+		int profit = maxRight - minLeft;
+		
+		return Math.max(Math.max(profitLeft, profitRight),profit);		
+	}
+	/*
+	public static int[] BuySellStockTopDown2(int[] prices, int indexLow, int indexHigh) {
+		if (indexLow >= indexHigh) return new int[] {0, prices[indexHigh], prices[indexHigh], 1};
+
+		int index_medium = (indexLow + indexHigh) / 2;
+		int[] resultLeft = BuySellStockTopDown2(prices, indexLow, index_medium);
+		int[] resultRight = BuySellStockTopDown2(prices, index_medium + 1, indexHigh);
+		
+		if (resultLeft[3] == 1) {
+				resultLeft[2] = Integer.MIN_VALUE;
+				resultLeft[3] = 0;
+		}
+		
+		if (resultRight[3] == 1) {
+			resultRight[1] = Integer.MAX_VALUE;
+			resultRight[3] = 0;
+		}
+		//in resultLeft e resultRight salvo:
+		//[0] = profitto
+		//[1] = Min
+		//[2] = Max
+		int profit = resultRight[2] - resultLeft[1];
+		
+		int min = resultLeft[1] < resultRight[1] ? resultLeft[1] : resultRight[1];
+		int max = resultLeft[2] > resultRight[2] ? resultLeft[2] : resultRight[2];
+		
+		return new int[] {(Math.max(Math.max(resultLeft[0], resultRight[0]),profit)), min,  max, 0};		
+	}*/
+	
 	// questa soluzione costa time O(n) space O(n)
 	public static int BuySellStockBottomUp(int[] prices) {
 		if (prices == null || prices.length <= 1) return 0;
@@ -65,6 +119,8 @@ public class BuySellStocks {
 	}
 	
 	public static void main (String[] args) {
+		System.out.println(BuySellStockTopDown(new int[] {7,1,5,3,6,4}, 0, 5));
+		//System.out.println(Arrays.toString(BuySellStockTopDown2(new int[] {7,1,5,3,6,4}, 0, 5)));
 		System.out.println(BuySellStockBottomUp(new int[] {7,1,5,3,6,4}));
 		System.out.println(BuySellStockBottomUp2(new int[] {7,1,5,3,6,4}));
 		System.out.println(BuySellStockBottomUp2(new int[] {}));
